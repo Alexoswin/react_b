e75147d13c3e8ab8875f517bf24451bccd213ef0
oswin <oswinalex1@gmail.com>
2023-09-28 01:57:07 +0530

    react DOM rendering elements

--- tut3_create_react_app/my-app/src/App.js
+++ tut3_create_react_app/my-app/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import './App1.css';
-import ReactDOM from 'react-dom';
-
+import './element1.js'
 
 
 function App() {
@@ -20,20 +19,4 @@ function App() {
 
 export default App;
 
-
-const root = ReactDOM.createRoot(
-  document.getElementById('root')
-);
-
-function tick() {
-  const element = (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}</h2>
-    </div>
-  );
-  root.render(element);
-}
-
-setInterval(tick, 1000);
 
