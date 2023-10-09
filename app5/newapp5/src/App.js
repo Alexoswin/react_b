16040d60e124e02dd811dd72782402f141834cf8
oswin <oswinalex1@gmail.com>
2023-10-09 22:08:21 +0530

    map

--- app5/newapp5/src/App.js
+++ app5/newapp5/src/App.js
@@ -1,45 +1,95 @@
-
 import './App.css';
+import React, { Component } from 'react'; // Import React and Component
+import mapboxgl from 'mapbox-gl'; // Import mapboxgl library
 
-function App() {
-  return (
+class App extends Component {
+  componentDidMount() {
+    mapboxgl.accessToken = 'pk.eyJ1Ijoib3N3aW5hbGV4IiwiYSI6ImNsbmV1endyMjBsNDEycXRjNmt4M2I3a28ifQ.M0WoyDbplPY3KfJKN5HZVg'; // Replace with your Mapbox access token
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [72.8777, 19.0760], // Default center (New York City)
+      zoom: 9.5
+    });
+
+    // Function to calculate the route
+    function calculateRoute() {
+      var startLocation = encodeURIComponent(document.getElementById('startLocation').value);
+      var endLocation = encodeURIComponent(document.getElementById('endLocation').value);
+
+      // Use a geocoding service to convert place names to coordinates
+      fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${startLocation}.json?access_token=${mapboxgl.accessToken}`)
+        .then(response => response.json())
+        .then(startData => {
+          var startCoordinates = startData.features[0].geometry.coordinates;
+
+          fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${endLocation}.json?access_token=${mapboxgl.accessToken}`)
+            .then(response => response.json())
+            .then(endData => {
+              var endCoordinates = endData.features[0].geometry.coordinates;
+
+              // Now, use the coordinates to fetch the route
+              fetch(`https://api.mapbox.com/directions/v5/mapbox/driving/${startCoordinates.join(',')};${endCoordinates.join(',')}?access_token=${mapboxgl.accessToken}`)
+                .then(response => response.json())
+                .then(data => {
+                  if (data.routes && data.routes.length > 0) {
+                    var distance = (data.routes[0].distance / 1000).toFixed(2); // Convert meters to kilometers
+                    document.getElementById('totalDistance').textContent = distance;
+                    var route = data.routes[0].geometry;
+                    if (!map.getSource('route')) {
+                      map.addSource('route', {
+                        type: 'geojson',
+                        data: {
+                          type: 'Feature',
+                          properties: {},
+                          geometry: {
+                            type: 'LineString',
+                            coordinates: []
+                          }
+                        }
+                      });
+                    }
+                    map.getSource('route').setData(route);
+                  } else {
+                    console.error('No route found.');
+                    document.getElementById('totalDistance').textContent = 'N/A';
+                  }
+                })
+                .catch(error => {
+                  console.error('Error fetching route data:', error);
+                  document.getElementById('totalDistance').textContent = 'N/A';
+                });
+            })
+            .catch(error => {
+              console.error('Error fetching end location coordinates:', error);
+              document.getElementById('totalDistance').textContent = 'N/A';
+            });
+        })
+        .catch(error => {
+          console.error('Error fetching start location coordinates:', error);
+          document.getElementById('totalDistance').textContent = 'N/A';
+        });
+    }
+
+    // Attach the calculateRoute function to the button click event
+    document.getElementById('calculateButton').addEventListener('click', calculateRoute);
+  }
+
+  render() {
+    return (
       <>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
-              <a class="navbar-brand" href="#">Navbar</a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul class="navbar-nav">
-                  <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">Features</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">Pricing</a>
-                  </li>
-                  <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                      Dropdown link
-                    </a>
-                    <ul class="dropdown-menu">
-                      <li><a class="dropdown-item" href="#">Action</a></li>
-                      <li><a class="dropdown-item" href="#">Another action</a></li>
-                      <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </nav>
-         
-
+        <div id="map"></div>
+        <div>
+          <label>Start Location:</label>
+          <input type="text" id="startLocation" placeholder="Enter start location" />
+          <label>End Location:</label>
+          <input type="text" id="endLocation" placeholder="Enter end location" />
+          <button id="calculateButton">Calculate Route</button>
+          <p>Total Distance: <span id="totalDistance">-</span> km</p>
+        </div>
       </>
-  );
+    );
+  }
 }
 
 export default App;
-  
