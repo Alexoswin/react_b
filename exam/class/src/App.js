10f7e8aff3fb05fe01a1de0783efc2c50423941c
oswin <oswinalex1@gmail.com>
2023-10-21 16:31:57 +0530

    class coponent

---
+++ exam/class/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import New from "./class.js"
+function App() {
+  return (<>
+
+   <New/>
+  </>
+   
+  );
+}
+
+export default App;
---
+++ exam/class/src/class.js
@@ -0,0 +1,14 @@
+
+import React , {Component } from 'react'
+
+class New extends Component{
+    render(){
+        
+            return(
+                <h1>hellow world
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum, facilis in eaque doloremque quibusdam nobis adipisci quo! Possimus doloremque corporis laboriosam soluta aut quod, velit earum est, dolore rem ad?
+                </h1>
+            )
+        }
+    }
+export default New
